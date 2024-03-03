2b80fe9f36e553274c656486e9173f78b266ae11
Tom1989 <thomas.walciszewski@gmail.com>
2024-03-03 18:15:11 +0100

    Creating and configuring Authorization server with Keycloak + creating and configuring OAuth Resource Server (starting with secured endpoints from REST controllers: authorization code/Token access flow)

---
+++ src/main/java/com/appsdeveloper/ws/api/ResourceServer/controllers/UsersController.java
@@ -0,0 +1,25 @@
+package com.appsdeveloper.ws.api.ResourceServer.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+	
+	@Autowired
+	Environment env;
+
+	@GetMapping("/status/check")
+	public String status() {
+		return " Successfully working  ";
+	}
+	
+
+	
+}
